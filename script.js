c5315d558c787e5d41da692303078cfe6b0680d0
brett burrington <bburrington13@gmail.com>
2023-07-12 14:17:50 -0700

    moved css out of assets to allow it to function

--- script.js
+++ script.js
@@ -1,51 +1,66 @@
-$(document).ready(function () {
-    var date = moment();
-    var block = $("#time-blocks");
-  
-    $("#currentDay").text(date.format("dddd MMM D, YYYY"));
-  
-    block.on("click", ".saveBtn", function (event) {
-      var hour = event.target.parentElement;
-      localStorage.setItem(hour.id, hour.children[1].value);
-    });
-  
-    function init() {
-      var currentTime = moment().hour();
-  
-      for (var i = 9; i <= 17; i++) {
-        var newDiv = $("<div>");
-        var hourLabel = "hour-" + i;
-        newDiv.attr("id", hourLabel);
-  //showing time
-        if (currentTime > i) {
-          newDiv.addClass("row time-block past");
-        } else if (currentTime === i) {
-          newDiv.addClass("row time-block present");
-        } else {
-          newDiv.addClass("row time-block future");
-        }
-  //color change for time of day
-        $("<div>")
-          .addClass("col-2 col-md-1 hour text-center py-3")
-          .text(i > 12 ? i - 12 + "PM" : i + "AM")
-          .appendTo(newDiv);
-  
-        $("<textarea>")
-          .addClass("col-8 col-md-10 description")
-          .attr("rows", "3")
-          .val(localStorage.getItem(hourLabel))
-          .appendTo(newDiv);
-  //properties of save button
-        $("<button>")
-          .addClass("btn saveBtn col-2 col-md-1")
-          .attr("aria-label", "save")
-          .append('<i class="fas fa-save" aria-hidden="true"></i>')
-          .appendTo(newDiv);
-  //append 
-        block.append(newDiv);
-      }
-    }
-  
-    init();
+$(document).ready(function() {
+  var date = moment(); // Get the current date using Moment.js
+  var block = $("#time-blocks"); // Select the element with the id "time-blocks"
+
+  $("#currentDay").text(date.format("dddd MMM D, YYYY")); // Set the text of the element with id "currentDay" to the formatted current date
+
+  block.on("click", ".saveBtn", function(event) {
+    // Event handler for click events on elements with class "saveBtn" inside the "time-blocks" container
+    var hour = event.target.parentElement; // Get the parent element of the clicked button
+    localStorage.setItem(hour.id, hour.children[1].value); // Store the value of the second child element of the parent element in the browser's local storage using the parent element's id as the key
   });
-  
+
+  function updateColors() {
+    // Update the colors of time blocks based on current time
+    var currentTime = moment().hour();
+
+    $(".time-block").each(function() {
+      var hour = parseInt($(this).attr("id").split("-")[1]);
+
+      $(this).removeClass("past present future");
+
+      if (currentTime > hour) {
+        $(this).addClass("past");
+      } else if (currentTime === hour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  function init() {
+    // Initialization function
+    for (var i = 9; i <= 17; i++) {
+      var newDiv = $("<div>");
+      var hourLabel = "hour-" + i;
+
+      newDiv.attr("id", hourLabel);
+      newDiv.addClass("row time-block");
+
+      $("<div>")
+        .addClass("col-2 col-md-1 hour text-center py-3")
+        .text(i > 12 ? i - 12 + "PM" : i + "AM")
+        .appendTo(newDiv);
+
+      $("<textarea>")
+        .addClass("col-8 col-md-10 description")
+        .attr("rows", "3")
+        .val(localStorage.getItem(hourLabel))
+        .appendTo(newDiv);
+
+      $("<button>")
+        .addClass("btn saveBtn col-2 col-md-1")
+        .attr("aria-label", "save")
+        .append('<i class="fas fa-save" aria-hidden="true"></i>')
+        .appendTo(newDiv);
+
+      block.append(newDiv);
+    }
+
+    updateColors(); // Call the updateColors function to set initial colors
+    setInterval(updateColors, 60000); // Update colors every minute
+  }
+
+  init(); // Call the initialization function
+});
