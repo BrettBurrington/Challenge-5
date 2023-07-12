d5d9c845470de79a146b7f5cbc0a2bdc9c574415
brett burrington <bburrington13@gmail.com>
2023-07-11 18:17:23 -0700

    fixed break

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    var date = moment();
+    var block = $("#time-blocks");
+  
+    $("#currentDay").text(date.format("dddd MMM D, YYYY"));
+  
+    block.on("click", ".saveBtn", function (event) {
+      var hour = event.target.parentElement;
+      localStorage.setItem(hour.id, hour.children[1].value);
+    });
+  
+    function init() {
+      var currentTime = moment().hour();
+  
+      for (var i = 9; i <= 17; i++) {
+        var newDiv = $("<div>");
+        var hourLabel = "hour-" + i;
+        newDiv.attr("id", hourLabel);
+  //showing time
+        if (currentTime > i) {
+          newDiv.addClass("row time-block past");
+        } else if (currentTime === i) {
+          newDiv.addClass("row time-block present");
+        } else {
+          newDiv.addClass("row time-block future");
+        }
+  //color change for time of day
+        $("<div>")
+          .addClass("col-2 col-md-1 hour text-center py-3")
+          .text(i > 12 ? i - 12 + "PM" : i + "AM")
+          .appendTo(newDiv);
+  
+        $("<textarea>")
+          .addClass("col-8 col-md-10 description")
+          .attr("rows", "3")
+          .val(localStorage.getItem(hourLabel))
+          .appendTo(newDiv);
+  //properties of save button
+        $("<button>")
+          .addClass("btn saveBtn col-2 col-md-1")
+          .attr("aria-label", "save")
+          .append('<i class="fas fa-save" aria-hidden="true"></i>')
+          .appendTo(newDiv);
+  //append 
+        block.append(newDiv);
+      }
+    }
+  
+    init();
+  });
+  
